0a78e964747af0e37e0c4661fac34c6a29d0a8d8
Naing Myo Thaw <naingmyothaw.nmyt@gmail.com>
2019-05-26 13:34:28 +0630

    Add a widget_test.dart file. Purpose is to build this project on codemagic successfully.

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_circle_wave_design/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+  });
+}
